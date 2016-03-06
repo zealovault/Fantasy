8f164b720482f1fa3cdbc292d9d6b0d8d14031fd
KinTek <biles.christopher@gmail.com>
2016-03-05 21:49:28 -0600

    Revert "Started Framework"
    This reverts commit 43a1db6b2cb700336988dca7fa6f247d632e2e4f.

--- Pilfer/src/base/Pilfer.java
+++ Pilfer/src/base/Pilfer.java
@@ -1,27 +1,13 @@
 package base;
 
 import base.utilities.*;
-import base.framework.*;
+
+import importedFiles.*;
 
 public class Pilfer {
-	
-	private static Block _block;
-	
-	private static int _randomBlockID;
-	private static int _gameState;
 
 	public static void main(String[] args) {
-		_gameState = 1;
 		
-		while (_gameState == GameState.Running.State()) {
-			_block = new Block();
-			_randomBlockID = RandomBlock.Block(0, 3);
-			
-			_block.SetBlockID(Blocks.Select(_randomBlockID).BlockID());
-			_block.SetBlockSprite(Blocks.Select(_randomBlockID).BlockSprite());
-			
-			System.out.println("Block ID: " + _block.GetBlockID() + "\r\n" + "Block Sprite: " + _block.GetBlockSprite());
-		}
 	}
 
 }
